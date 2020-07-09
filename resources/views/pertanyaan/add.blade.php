e20b2365ae87ddaad8c9d26ce5aa0ccf47c3ca7c
setrom <setrom@gmail.com>
2020-07-10 01:31:20 +0700

    ckeditor

--- resources/views/pertanyaan/add.blade.php
+++ resources/views/pertanyaan/add.blade.php
@@ -9,11 +9,17 @@
             </div>
 
             <article class="question question-type-normal">
+                   <form action="{{ url('artikel')}}" method="POST">
+            @csrf
+            <input type="hidden" name="userid" value="1">
                 <div class="form-group">
                     <label>CONTENT</label>
                     <textarea class="form-control" name="pertanyaan" id="pertanyaan" placeholder="Masukkan Content" rows="4"></textarea>
                 </div>
+                 <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
             </article>
+
 
 
         </div>
@@ -23,5 +29,15 @@
 
 
 @push('scripts')
+<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+<script src="//cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
+<script>
+$(document).ready(function() {
+    CKEDITOR.replace('pertanyaan');
+});
 
+</script>
 @endpush
