b0209684c776bb1af128c1d5532529721ef6acf8
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2020-07-09 23:13:34 +0700

    Fix the relations of likedisjawaban and likedispertanyaan table

--- database/migrations/2020_07_08_161038_create_likedispertanyaan_table.php
+++ database/migrations/2020_07_08_161038_create_likedispertanyaan_table.php
@@ -14,11 +14,12 @@ class CreateLikedispertanyaanTable extends Migration
     public function up()
     {
         Schema::create('likedispertanyaan', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->unsigned();
-            $table->unsignedBigInteger('pertanyaan_id')->unsigned();
-            $table->integer('value')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('pertanyaan_id')->references('id')->on('pertanyaan');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('pertanyaan_id');
+            $table->tinyInteger('value');
+            $table->primary(['user_id', 'pertanyaan_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('pertanyaan_id')->references('id')->on('pertanyaan')->onDelete('cascade');
         });
     }
 
