0acf53839bdd093ef5006241507ae2e5a76e06c1
Boy Gautama <boy.gautama@gmail.com>
2020-07-12 16:37:12 +0700

    profil

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Profile</h1>
+          </div>
+          <div class="col-sm-6">
+         
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+    @endsection
