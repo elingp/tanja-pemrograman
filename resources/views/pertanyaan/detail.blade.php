c0a02dd9cf64994c622d0a21badf70958ce30cbc
Boy Gautama <boy.gautama@gmail.com>
2020-07-12 10:49:31 +0700

    halaman detail

--- resources/views/pertanyaan/detail.blade.php
+++ resources/views/pertanyaan/detail.blade.php
@@ -1,6 +1,171 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Detail Pertanyaan</h1>
+          </div>
+          <div class="col-sm-6">
+              <span class="float-right"><a class="btn btn-primary btn-sm" href="/pertanyaan/create" role="button"><i class="fa fa-question-circle" aria-hidden="true"></i> Buat Pertanyaan</a></span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+<section class="content">
+     <div class="card card-widget">
+     <div class="card-header">
+                <div class="user-block">
+                  <img class="img-circle" src="{{ asset('img/avatar_m.png')}}" alt="User Image">
+                  <span class="username"><a href="#" class="text-muted">{{ $question->user->name }}</a></span>
+                  <span class="description text-bold"><h3> <a href="/pertanyaan/{{ $question->id }}/{{ $question->slug }}">{{ $question->judul }}</a></h3></span>
+                  <span class="description text-sm">
+                <span class="badge bg-primar link-black text-sm"><small><i class="far fa-thumbs-up"></i> Like ({{ $question->likedislikes->sum('value') }})</small></span>
+               <span class="badge bg-primar link-black text-sm"><small><i class="far fa-comment"></i> {{ $question->created_at->diffForHumans() }}</small></span>
+               <span class="badge bg-primar link-black text-sm"><small><i class="far fa-comments"></i> {{ $question->comments->count() }} answers</small></span>
+               <span class="badge bg-primar link-black text-sm"><small><i class="far fa-eye"></i> {{ $question->view }} views</small></span>
+      
+                </span>
+                </div>
+                <!-- /.user-block -->
+                <div class="card-tools">
+                </div>
+                <!-- /.card-tools -->
+              </div>
+ <div class="card-body">
+  {!! $question->isi !!}
+  <hr>
+ @if (!empty($question->tag))
+                        @foreach (explode(' ',$question->tag) as $item)
+                        <span class="badge badge-primary">{{$item}}</span>
+                        @endforeach
+                        @endif          
+
+
+
+
+@if (!empty($question->comments))
+<br>
+<hr>
+
+
+                        @foreach ($question->comments as $komentanya)
+                        {{-- <li class="comment pt-3"><small>
+                                <p>{!! $komentanya->isi !!}. </p>
+                                {{$komentanya->user->name}} | <i class="far fa-clock"></i> {{ $komentanya->created_at->diffForHumans() }}
+                            </small>
+                        </li> --}}
+                        <div class="direct-chat-msg">
+                    <div class="direct-chat-infos clearfix">
+                      <span class="direct-chat-name float-left"> {{$komentanya->user->name}}</span>
+                      <span class="direct-chat-timestamp float-right">{{ $komentanya->created_at->diffForHumans() }}</span>
+                    </div>
+                    <!-- /.direct-chat-infos -->
+                    <img class="direct-chat-img" src="{{ asset('img/avatar_m.png')}}" alt="Message User Image">
+                    <!-- /.direct-chat-img -->
+                    <div class="direct-chat-text">
+                      {!! $komentanya->isi !!}
+                    </div>
+                    <!-- /.direct-chat-text -->
+                  </div>
+                        @endforeach
+
+                    @endif
+</div>
+
+</section>
+
+
+<section class="content">
+     <div class="card card-widget">
+     <div class="card-header"><h5 class="header-title">Jawaban ( <span class="color">{{ $question->jawaban->count() }}</span> )</h5></div>
+     <div class="card-body">
+  @if ($question->jawaban->count() > 0)
+                    
+                        @foreach ($question->jawaban as $answer)
+                       <div class="post">
+                      <div class="user-block">
+                        <img class="img-circle img-bordered-sm" src="{{ asset('img/avatar_m.png')}}" alt="user image">
+                        <span class="username">
+                          <a href="#">{{ $answer->user->name }}</a>
+                          @if (Auth::user()->name== $answer->user->name)
+                              <button type="button" class="btn btn-danger btn-xs float-right"><i class="far fa-trash-alt"></i> Hapus</button>
+                          @endif
+                        </span>
+                        <span class="description">{{ $answer->created_at->diffForHumans() }}</span>
+                      </div>
+                      <!-- /.user-block -->
+                      <p>
+                         {!! $answer->isi !!}
+                      </p>
+
+                      <p>
+                        <a href="#" class="link-black text-sm mr-2"><i class="fas fa-share mr-1"></i> Share</a>
+                        <a href="#" class="link-black text-sm"><i class="far fa-thumbs-up mr-1"></i> Like</a>
+                        <span class="float-right">
+                          <a href="#" class="link-black text-sm">
+                            <i class="far fa-comments mr-1"></i> Comments (5)
+                          </a>
+                        </span>
+                      </p>
+
+                      {{-- <input class="form-control form-control-sm" type="text" placeholder="Type a comment"> --}}
+                      <div class="input-group input-group-sm">
+                  <input type="text" class="form-control" placeholder="Type a comment">
+                  <span class="input-group-append">
+                    <button type="button" class="btn btn-info btn-flat">Go!</button>
+                  </span>
+                </div>
+                    </div>
+                        @endforeach
+                   
+                @else
+                    <p>Belum ada jawaban! Jadilah yang pertama menjawab pertanyaan ini.</p>
+                @endif
+     </div>
+     </div>
+</section>
+
+<section class="content">
+     <div class="card card-widget">
+     <div class="card-header"><h5 class="header-title">Jawaban Anda</h5></div>
+     <div class="card-body">
+
+@auth
+               
+                <form action="/jawaban" method="POST">
+                    @csrf
+                    <input type="hidden" name="penjawab_id" value="{{ Auth::id() }}">
+                    <input type="hidden" name="pertanyaan_id" value="{{ $question->id }}">
+                    <div class="form-group">
+                        <label for="isi" class="sr-only">Isi</label>
+                        <textarea class="form-control @error('isi') is-invalid @enderror" name="isi" id="isi" rows="4" required>{{ old('isi') }}</textarea>
+                        @error('isi')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">Kirim Jawaban</button>
+                </form>
+                @endauth
+                @guest
+                <a href="/login">
+                    <div class="alert-message error">
+                        <i class="fas fa-lock"></i>
+                        <p><span>Login</span><br>
+                            Anda harus Login untuk menjawab!</p>
+                    </div>
+                </a>
+                @endguest
+     </div>
+     </div>
+</section>
+
+
+{{-- 
 
 <div class="content-wrapper ">
     <div class="row justify-content-center">
@@ -77,7 +242,7 @@
 <div class="card-footer">
                 
               </div>
-</section>
+</section> --}}
 @endsection
 
 @push('scripts')
--- resources/views/pertanyaan/index.blade.php
+++ resources/views/pertanyaan/index.blade.php
@@ -10,8 +10,7 @@
             <h1>Daftar Pertanyaan</h1>
           </div>
           <div class="col-sm-6">
-            <span class="float-right">
-            <a class="btn btn-primary btn-md" href="/pertanyaan/create" role="button"><i class="fa fa-question-circle" aria-hidden="true"></i> Buat Pertanyaan</a></span>
+            <span class="float-right"><a class="btn btn-primary btn-sm" href="/pertanyaan/create" role="button"><i class="fa fa-question-circle" aria-hidden="true"></i> Buat Pertanyaan</a></span>
           </div>
         </div>
       </div><!-- /.container-fluid -->
@@ -29,10 +28,9 @@
                 </div>
                 <!-- /.user-block -->
                 <div class="card-tools">
-                 <span data-toggle="tooltip" title={{ $question->created_at->diffForHumans() }}" class="badge bg-danger">{{ $question->created_at->diffForHumans() }}</span>
+                 <span data-toggle="tooltip" title={{ $question->created_at->diffForHumans() }}" class="badge bg-info">{{ $question->created_at->diffForHumans() }}</span>
                   <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                   </button>
-
                 </div>
                 <!-- /.card-tools -->
               </div>
@@ -40,14 +38,13 @@
               <div class="card-body">
                 <!-- post text -->
                {!! Illuminate\Support\Str::limit(strip_tags($question->isi), 200) !!}
-<hr>
-
-
-
-                <span class="float-left">
+              </div>
+              <!-- /.card-footer -->
+              <div class="card-footer">
+                 <span class="float-left">
                       @if (!empty($question->tag))
                         @foreach (explode(' ',$question->tag) as $item)
-                         <span class="badge badge-primary">{{$item}}</span>
+                         <span class="badge badge-success">{{$item}}</span>
                         @endforeach
                         @endif
                 </span>
@@ -55,22 +52,7 @@
                <span class="float-right badge bg-primar link-black text-sm"><i class="far fa-comment"></i> {{ $question->created_at->diffForHumans() }}</span>
                <span class="float-right badge bg-primar link-black text-sm"><i class="far fa-comments"></i> {{ $question->comments->count() }} answers</span>
                <span class="float-right badge bg-primar link-black text-sm"> <i class="far fa-eye"></i> {{ $question->view }} views</span>
-
-
-
-
               </div>
-
-              <!-- /.card-footer -->
-              {{-- <div class="card-footer">
-                <form action="#" method="post" _lpchecked="1">
-                  <img class="img-fluid img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="Alt Text">
-                  <!-- .img-push is used to add margin to elements next to floating images -->
-                  <div class="img-push">
-                    <input type="text" class="form-control form-control-sm" placeholder="Press enter to post comment">
-                  </div>
-                </form>
-              </div> --}}
               <!-- /.card-footer -->
 </div>
     @endforeach
@@ -79,6 +61,6 @@
   @else
                 <p>Belum ada pertanyaan!</p>
             @endif
-        </div>
+
  </section>
 @endsection
